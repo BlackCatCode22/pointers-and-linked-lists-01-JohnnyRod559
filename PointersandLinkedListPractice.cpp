c84b7c94b655c76a4afec235d86ce62d1610532a
JohnnyRod559 <141672746+JohnnyRod559@users.noreply.github.com>
2023-08-30 11:17:51 -0700

    updated

--- PointersandLinkedListPractice.cpp
+++ PointersandLinkedListPractice.cpp
@@ -13,7 +13,7 @@ int main() {
     cout << "\n Welcome " << userInput << "! " << endl;
 
     int num1 = 5;
-    cout << "\n" << &n << endl;
+    cout << "\n" << &num1 << endl;
 
     int* ptr = &num1;
     cout << ptr << endl;
@@ -23,9 +23,25 @@ int main() {
     cout << num1 << endl;
 
     int num2;
-    int* ptr2 =&v;
+    int* ptr2 =&num2;
     *ptr2 =7;
     cout << "num2= " << num2 << endl;
+    cout <<"\n\n";
+
+    int age = 19;
+    int* pAge = &age;
+    double gpa = 2.7;
+    double* pGpa = &gpa;
+    string name = "Johnny";
+    string* pName = &name;
+
+    cout << "Age: " << &age << endl;
+    cout << "gpa: " << &gpa << endl;
+    cout << "name: " << &name << endl << endl;
+
+    cout << pAge << endl;
+
+    cout << *pAge << endl;
 
 
 
